9c1c582191bf8c63f8b9486d275f26847233f47a
MariaNattestad <lunexa@gmail.com>
2016-05-30 18:49:57 -0700

    small fix

--- file_upload.php
+++ file_upload.php
@@ -1,8 +1,17 @@
 <?php
     
+    
     $code = escapeshellarg($_POST["code_hidden"]);
-    $name = "./user_uploads/" . $code;
+    $file_type = $_POST["file_type"];
+    $name = "./user_uploads/" . $code . "." . $file_type;
     
     move_uploaded_file($_FILES['file']['tmp_name'], $name);
-    
+
+    ////for debugging:
+    //file_put_contents( 'yowtf', print_r($_POST["code_hidden"], true));
+    //
+    //
+    //file_put_contents( 'yohai', print_r($name, true));
+    //
+     
 ?>
