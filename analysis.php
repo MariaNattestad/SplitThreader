10a352804ced278c1142e06d9e473f1297c7eeff
Maria Nattestad <maria.nattestad@gmail.com>
2016-12-01 09:42:53 -0800

    Update URL to splitthreader.com

--- analysis.php
+++ analysis.php
@@ -48,7 +48,7 @@
           <div id="code" class="panel-body">
             <?php
                 $code=$_GET["code"];
-                $url="http://qb.cshl.edu/splitthreader/analysis.php?code=$code";
+                $url="http://splitthreader.com/analysis.php?code=$code";
     
                 echo "Return to view your results at any time: <input type=\"text\" class=\"form-control\" value=\"$url\"></input>";
             ?>
